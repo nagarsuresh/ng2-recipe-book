c1f8035de4cafbb2a4db111b25ee85a5b504ac1f
Suresh Nagar <nagar.suresh@gmail.com>
2017-02-03 22:52:35 -0800

    recipe book - from udemy tutorial by max

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ng2-recipe-book
+
+From https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/overview
---
+++ src/app/recipes/recipe.ts
@@ -0,0 +1,5 @@
+import { Ingredient } from "../shared";
+
+export class Recipe {
+  constructor(public id:number, public name:string, public description:string, public imagePath:string, public ingredients: Ingredient[]) {}
+}
